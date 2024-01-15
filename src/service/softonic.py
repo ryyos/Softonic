087f06d595ac31ecb51d9d4c6c60a8abe71ce03f
ryosyntaxx <riodwi12174@gmail.com>
2024-01-15 17:43:00 +0700

    done create fetch game

--- src/service/softonic.py
+++ src/service/softonic.py
@@ -21,6 +21,14 @@ class Softonic:
         self.MAIN_DOMAIN = 'en.softonic.com'
         self.MAIN_URL = 'https://en.softonic.com/'
 
+        self.TYPES = [
+            "new-apps",
+            "trending",
+            "date",
+            "last-news",
+            "new-versions"
+        ]
+
         ...
 
     def __retry(self, url: str, 
@@ -35,9 +43,10 @@ class Softonic:
                         response = requests.get(url)
                         ic(response)
 
-                        if response.status_code == 200: break
-                        if response.status_code == 500: break
-                        if response.status_code == 403: break
+                        if response.status_code == 200: return response
+                        if response.status_code == 500: return response
+                        if response.status_code == 403: return response
+                        if response.status_code == 404: return response
 
                         sleep(retry_interval)
                         retry_interval+=5
@@ -49,7 +58,6 @@ class Softonic:
                         ...
                     ...
                 
-                return response
                 ...
 
             case 'post':
@@ -61,6 +69,7 @@ class Softonic:
                         if response.status_code == 200: break
                         if response.status_code == 500: break
                         if response.status_code == 403: break
+                        if response.status_code == 404: break
 
                         sleep(retry_interval)
                         retry_interval+=5
@@ -72,20 +81,56 @@ class Softonic:
                         ...
                     ...
                 ...
+        ...
 
+
+    def __fetch_game(self, url: str):
+        response = self.__retry(url=url, action='get')
+        
+        games = []
+        page = 1
+        while True:
+            html = PyQuery(response.text)
+
+            for game in html.find('a[data-meta="app"]'): games.append(PyQuery(game).attr('href'))
+
+            response: Response = self.__retry(url=f'{url}/{page}', action='get')
+            page+=1
+
+            if response.status_code != 200: break
+            if not html.find('a[data-meta="app"]'): break
+
+            break
+
+            ...
+        ic(games)
         ...
 
     def __fetch_categories(self, url: str) -> List[str]:
         response: Response = self.__retry(url=url, action='get')
         html = PyQuery(response.text)
-
+    
         categories_urls = [PyQuery(categories).attr('href') for categories in html.find('#sidenav-menu a[class="WNTOdu tEDxqA js-menu-categories-item"]')]
         
-        self.__file.write_json(path='private/categories.json', content=categories_urls)
-
-        ...
+        return categories_urls
+        
 
     def main(self):
-        self.__fetch_categories(url=self.MAIN_URL)
+        categories_urls = self.__fetch_categories(url=self.MAIN_URL)
+
+        for categories in categories_urls:
+            for type in self.TYPES:
+
+                ic(categories)
+                ic(type)
+                self.__fetch_game(url=f'{categories}:{type}')
+
+                break
+                ...
+            break
+            ...
 
         ...
+
+
+# data/data_raw/softonic_review/action/trending/name_app/file.json
